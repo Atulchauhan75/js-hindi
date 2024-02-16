b907238726795d60303cecc717fe0a44dce3c4b3
Atulchauhan75 <atulchauhan1903@gmail.com>
2024-02-16 16:51:59 +0530

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+alert("hello you are ot to be here"); //we are using node js, not browser
+console.log(3 + 
+    3) //code readability should be high
+    let name="atul"//string datatype
+    let age =20 //int datatype
+    let isCheckedIn =false//boolean datatype
+
+
+    //types of datatype
+    //number => 2 to power 53
+    //bigint for large numbers
+    //string => ""
+    //boolean => true/false
+    //null =>standalone value
+    //undefined => value is not assigned yet to the variable
+    //symbol => helps in finding uniqueness
+
+
+    //object
+
+    console.log( typeof undefined) // undefined
+    console.log( typeof null) //object
+
