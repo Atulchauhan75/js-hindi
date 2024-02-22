c6b106a03887e4c5aa705452bd1255ef9e59623c
Atulchauhan75 <atulchauhan1903@gmail.com>
2024-02-22 20:43:24 +0530

    json talk and destructuring of objects

---
+++ 02_basics/05_objects_destructure.js
@@ -0,0 +1,93 @@
+/* Destructuring assignment in JavaScript is a convenient way of extracting multiple values from data stored in objects or arrays and assigning them to variables. It provides a shorter syntax compared to traditional property access.*/
+
+// // Object to destructure
+// const person = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     age: 30,
+//     city: 'New York'
+//   };
+  
+//   // Destructuring assignment
+//   const { firstName, lastName, age, city } = person;
+  
+//   // Using the destructured values
+//   console.log(firstName); // Output: John
+//   console.log(lastName); // Output: Doe
+//   console.log(age); // Output: 30
+//   console.log(city); // Output: New York
+
+
+
+
+
+// // Object to destructure
+// const person = {
+//     firstName: 'John',
+//     lastName: 'Doe',
+//     age: 30,
+//     city: 'New York'
+//   };
+  
+//   // Destructuring assignment with variable renaming
+//   const { firstName: first, lastName: last } = person;
+  
+//   // Using the destructured values with renamed variables
+//   console.log(first); // Output: John
+//   console.log(last); // Output: Doe
+
+  
+
+
+
+
+// Object to destructure
+// const person = {
+//     firstName: 'John',
+//     age: 30
+//   };
+  
+//   // Destructuring assignment with default value
+//   const { firstName, lastName = 'Doe', age, city = 'Unknown' } = person;
+  
+//   // Using the destructured values with default values
+//   console.log(firstName); // Output: John
+//   console.log(lastName); // Output: Doe
+//   console.log(age); // Output: 30
+//   console.log(city); // Output: Unknown
+//   console.log(person);// Output: { firstName: 'John', age: 30 }
+  
+
+
+
+
+//Concept of API's
+
+/*previously we got every values from api in XML structure but nowdays we will get these values in json structure because xaml structure is so complex to understand*/
+
+//INTRODUCTION TO JSON API's
+// in json keys and values both are strings
+// when we fetch api we got objects or arrays of object and we can deal with them further
+// {
+//     "name" : "atul",
+//     "age" : "21",
+//     "price" : "free"
+// }
+
+//if you want to see some result after calling an api then ctrl+click on the link given below
+// https://api.github.com/users/Atulchauhan75
+
+// arrays of object
+[
+    {},
+    {},
+    {},
+]
+
+
+
+// use the website https://randomuser.me/ to chwck random api's
+// ctrl+click on the link given below 
+// https://randomuser.me/ and copy all code then paste it on the website json formatter via link 
+// https://jsonformatter.org/ and click on format and beautify to see code text treee structure for better understading
+
